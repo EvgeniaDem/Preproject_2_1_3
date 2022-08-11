4bb024e40635ed94b8d045b3e7c0ecbc123cfe7a
Евгения <jane_79@list.ru>
2022-08-11 23:13:58 +0300

    1st commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,29 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("animalCageBean")
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dogBean")
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer(){
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(new Timer().getTime()); // каждый раз создается новый объект
+        System.out.println(timer.getTime());       // bean singleton - один раз создался и все, он неизменный
         System.out.println("________________________");
     }
 }
